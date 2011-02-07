e49bf936e9295ab9878b5d7bc98bfbcb7796ec8e
Ryan Scherle <ryan@scherle.org>
2011-02-07 13:25:38 -0500

    HAMR time

---
+++ SimpleHamr.java
@@ -0,0 +1,17 @@
+
+class SimpleHamr extends Object {
+    public static void main(String[] args) {
+	// get a Dryad ID
+	String did = args[0];
+	System.out.println("processing " + did);
+
+	// retrieve the object from Dryad (in DIM format)
+
+	// extract its DOI
+
+	// get the corresponding record from crossref
+
+	// match fields, and add 
+	
+    }
+}
